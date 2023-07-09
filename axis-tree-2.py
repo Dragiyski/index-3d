d6b6916b15d3de0aeb5d52c24ff4a3a4510d3acf
Plamen Dragiyski <plamen@dragiyski.org>
2023-07-09 13:00:47 +0200

    Index triangle separation into 2-3 split tree.

--- axis-tree-2.py
+++ axis-tree-2.py
@@ -23,58 +23,91 @@ def dot(x, y):
 
 def compile_node(triangle_data, selection_index, parent = None):
     triangle_list = triangle_data[selection_index]
-    triangle_flat_vertex_list = triangle_list.position.reshape(-1, 3)
+    vertex_list = triangle_list.position.reshape(-1, 3)
     bounding_box = numpy.concatenate([
-        triangle_flat_vertex_list.min(axis = 0)[None],
-        triangle_flat_vertex_list.max(axis = 0)[None]
+        vertex_list.min(axis = 0)[None],
+        vertex_list.max(axis = 0)[None]
     ], axis = 0)
-    if selection_index.shape[0] < 6:
-        return Data(
-            is_container = False,
-            bounding_box=bounding_box,
-            index = selection_index,
-            depth = 0 if parent is None else (parent.depth + 1),
-            parent = parent
-        )
-    split_factor = numpy.median(triangle_flat_vertex_list, axis=0)
-    below_mask = triangle_list.position <= split_factor
-    above_mask = triangle_list.position >= split_factor
-    below_triangle_mask = numpy.any(below_mask, axis=1)
-    above_triangle_mask = numpy.any(above_mask, axis=1)
-    mixed_triangle_mask = numpy.logical_and(below_triangle_mask, above_triangle_mask)
-    mixed_distribution = numpy.count_nonzero(mixed_triangle_mask, axis=0)
-    for selected_axis in numpy.argsort(mixed_distribution):
-        axis_below_triangle_mask = below_triangle_mask[:, selected_axis]
-        axis_above_triangle_mask = above_triangle_mask[:, selected_axis]
-        axis_mixed_triangle_mask = mixed_triangle_mask[:, selected_axis]
-        axis_below_pure_triangle_mask = numpy.logical_and(axis_below_triangle_mask, numpy.logical_not(axis_mixed_triangle_mask))
-        axis_above_pure_triangle_mask = numpy.logical_and(axis_above_triangle_mask, numpy.logical_not(axis_mixed_triangle_mask))
-        axis_distribution = numpy.array([
-            numpy.count_nonzero(axis_below_pure_triangle_mask),
-            numpy.count_nonzero(axis_above_pure_triangle_mask),
-            numpy.count_nonzero(axis_mixed_triangle_mask),
-        ])
-        if axis_distribution[0] == 0 or axis_distribution[1] == 0:
-            continue
-        node = Data(
-            is_container = True,
-            bounding_box = bounding_box,
-            triangle_index = selection_index,
-            index = selection_index,
-            axis = selected_axis,
-            split_factor = split_factor[selected_axis],
-            depth = 0 if parent is None else (parent.depth + 1),
-            parent = parent
-        )
-        print('%d -> %d: [%d, %d, %d]' % (node.depth, selected_axis, *axis_distribution))
-        below_child = compile_node(triangle_data, selection_index[axis_below_triangle_mask], node)
-        above_child = compile_node(triangle_data, selection_index[axis_above_triangle_mask], node)
-        if below_child is not None and above_child is not None:
-            node.children = [below_child, above_child]
-            return node
-    else:
-        print('What Now?')
+    node = Data(
+        bounding_box = bounding_box,
+        index = selection_index,
+        depth = 0 if parent is None else (parent.depth + 1),
+        parent = parent
+    )
+    if node.depth >= 256:
+        raise RuntimeError('Index tree too deep: > 256 levels')
+    if selection_index.shape[0] <= 6:
+        node.is_container = False
+        print('> %d: %d' % (node.depth, selection_index.shape[0]))
+        return node
+    
+    split_factor_list = numpy.concatenate([
+        numpy.median(vertex_list, axis = 0)[None],
+        ((bounding_box[1] + bounding_box[0]) * 0.5)[None],
+        numpy.mean(vertex_list, axis = 0)[None]
+    ])
+    vertex_split_diff_list = triangle_list.position[:, :, None] - split_factor_list
+    # Shape:
+    # 3 groups: below plane, above plane, at plane
+    # N triangles:
+    # 3 vertex per triangle
+    # 3 split factors
+    # 3 dimensions
+    vertex_split_factor_mask = numpy.concatenate([
+        (vertex_split_diff_list <= -epsilon)[None],
+        (vertex_split_diff_list >= +epsilon)[None],
+        (numpy.abs(vertex_split_diff_list) < epsilon)[None]
+    ], axis = 0)
+    # This gives triangles with *all* vertices below, above, and *all* vertices *at* the plane, which is most likely zero (0).
+    # The sum will be <= number of triangles.
+    triangle_split_factor_mask = numpy.all(vertex_split_factor_mask, axis = 2)
+    # We reassign the group[2] into the mask of all triangles not in group 0 and 1, i.e. those that are split.
+    triangle_split_factor_mask[2] = numpy.logical_and(numpy.logical_not(triangle_split_factor_mask[0]), numpy.logical_not(triangle_split_factor_mask[1]))
+    # 3 split factors
+    # 3 dimensions
+    # 3 groups: below plane, above plane, at plane
+    distribution_list = numpy.moveaxis(numpy.count_nonzero(triangle_split_factor_mask, axis = 1), 0, -1)
     pass
+
+    # There is no easy way to split the volume in two along the axis and keep the split triangles at zero.
+    # But we do not need to do that to implement bounding box optimization.
+    # SOLUTION: 3 children nodes:
+    # First 2 children is what we expect, they divide the node volume in 2 and include triangles fully contained below or above the splitting plane.
+    # The bounding boxes of those two shapes is subset of (or match) the bounding box in the parent up to the split location of the chosen axis.
+    # The last children is a subset of (or match) the volume defined by the bounding box in the parent, but even if it matches, it will contain fewer triangles
+    # than the parent.
+    # First 2 children are expected to have nearly equivalent set of triangles (usually by the mean),
+    # if any of those children contains too few triangles we might reject the splitting method or axis.
+    # This is especially important if group[2] contains all the triangles: distribution [0, 0, N] will lead to infinite loop (so does [N, 0, 0] or [0, N, 0])
+    # A variant [1, 1, N] with large N can also be undesirable, as it can lead to large depth of the generated tree.
+    # Ideally, min(distribution) should be > 0, and min(distribution[0], distribution[1]) > max(distribution) / max_depth
+    # For example, max_depth = 256, with splitting 8530 triangles means  8530 // 256 = 33. Any divisition where group[0] or group[1] < 33, will be bad.
+    for distribution_flat_index in numpy.argsort(distribution_list[:, :, 2] + numpy.abs(distribution_list[:, :, 1] - distribution_list[:, :, 0]), axis=None):
+        split_factor_index = distribution_flat_index // 3
+        dimension_index = distribution_flat_index % 3
+        split_factor = split_factor_list[split_factor_index, dimension_index]
+        triangle_mask = triangle_split_factor_mask[:, :, split_factor_index, dimension_index]
+        distribution = distribution_list[split_factor_index, dimension_index]
+        children = []
+        for group_index in range(3):
+            group_list = numpy.nonzero(triangle_mask[group_index])[0]
+            if group_list.shape[0] > 0:
+                if numpy.setdiff1d(selection_index, selection_index[group_list]).shape[0] <= 0:
+                    # A child node must be a proper subset.
+                    # If a child contains all triangles, since each triangle is exactly in one child, other children would be empty.
+                    # As a result, this distribution would be denied and the loop will try another distribution.
+                    continue
+                child = compile_node(triangle_data, selection_index[group_list], node)
+                if child is not None:
+                    children.append(child)
+        # We only consider a node where triangles are split into two or more (namely, 3) children, each of them will be non-empty.
+        if len(children) >= 2:
+            node.children = children
+            node.split_factor = split_factor
+            node.distribution = distribution
+            print('+ %d: %r' % (node.depth, list(node.distribution)))
+            return node
+    return None
 
 
 def _main():
